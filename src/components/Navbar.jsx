5b6a1561c63637af739ceeea5da9fa2f86b7ada2
Maroof Qureshi <developermaroof@gmail.com>
2024-06-07 14:18:43 +0500

    Updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,13 +7,23 @@ import HamburgerOpen from '../assets/HamburgerOpen.png'
 
 const Navbar = () => {
   return (
-    <div className='Navbar bg-dark-theme flex'>
-    <div className="content">
-    <div className='logdiv'><img src={Logo} alt="" /></div>
-    <div className='modesdiv'><img src={ModeBlack} alt="" /> <span>Modes</span> <img src={DownArrow} alt="" /></div>
-  <div className='Hamburger'><img src={HamburgerOpen} alt="" /></div>
-  </div>
-    </div>
+    <nav className='Navbar bg-dark-theme flex justify-center items-center py-6'>
+      <div className="content flex items-center justify-between w-full px-4">
+        <div className='logdiv'>
+          <img src={Logo} alt="Company Logo" className="logo-img w-24 h-auto" />
+        </div>
+        <div className='modesdiv flex items-center justify-center gap-1 ml-10'>
+          <img src={ModeBlack} alt="Mode Icon" className="mode-icon w-3 h-auto" />
+          <span className='text-white text-xs'>Modes</span>
+          <img src={DownArrow} alt="Dropdown Arrow" className="down-arrow w-2 h-auto" />
+        </div>
+        <div>
+        <button className='Hamburger'>
+          <img src={HamburgerOpen} alt="Menu" className="hamburger-icon w-6 h-auto" />
+        </button>
+        </div>
+      </div>
+    </nav>
   )
 }
 
