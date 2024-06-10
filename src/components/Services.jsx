93bea57fcb2813fe0a0f508ced31e3d06891fa2c
Maroof Qureshi <developermaroof@gmail.com>
2024-06-10 15:39:12 +0500

    Updated

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -6,7 +6,7 @@ const Services = () => {
     <div className='Services bg-darker-theme gap-4 py-16 p-6 justify-center flex flex-col text-center items-center'>
       <p className='text-xs font-thin font-josefin'>NOT TO MENTION
       </p>
-      <h1 className='font-bold text-2xl'>Our Services</h1>
+      <h1 className='font-bold text-2xl font-josefin'>Our Services</h1>
       <p className='text-xs font-thin font-josefin'>Discover our comprehensive range of IT services designed to transform your business. From custom software development to SaaS solutions, our experienced team delivers excellence tailored to your specific needs. Collaborate with us to unlock the full potential of your business.</p>
       <img src={ServiceImg} alt="" />
     </div>
--- src/components/Transform.jsx
+++ src/components/Transform.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Transform = () => {
   return (
-    <div>Transform</div>
+    <div className='Transform bg-darker-theme gap-4 py-16 p-6 justify-center flex flex-col text-center items-center'>
+      <h1 className='font-bold text-sm font-josefin'>Transforming businesses through exceptional IT solutions
+
+</h1>
+      
+    </div>
   )
 }
 
