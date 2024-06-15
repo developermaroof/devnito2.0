a5b8ad023d7310570bf68226685599f091a839f0
Maroof Qureshi <developermaroof@gmail.com>
2024-06-15 12:36:26 +0500

    Updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,27 +3,27 @@ import React from 'react';
 const Contact = () => {
   return (
     <div className='Contact gap-10 py-12 px-6 flex flex-col justify-center items-center text-center'>
-      <h1 className='font-bold text-xl font-josefin'>Contact DevNito</h1>
-      <div className='bg-lighter-theme px-6 pb-10 flex flex-col gap-5 justify-center items-center text-center'>
-        <h1 className='bg-darker-theme text-xs font-josefin px-5 py-4 rounded-b-2xl'>Get in Touch with Us</h1>
-        <form className='gap-4 flex flex-col'>
+      <h1 className='font-bold text-xl font-josefin sm:text-2xl md:text-3xl'>Contact DevNito</h1>
+      <div className='bg-lighter-theme px-6 sm:px-12 md:px-20 pb-10 flex flex-col gap-5 md:gap-10 justify-center items-center text-center'>
+        <h1 className='bg-darker-theme text-xs sm:text-sm md:text-lg font-josefin px-5 py-4 rounded-b-2xl'>Get in Touch with Us</h1>
+        <form className='gap-4 md:gap-6 flex flex-col'>
           <div>
-            <input className='bg-dark-theme  placeholder-textgray py-2 pl-4 font-josefin text-xs outline-none' placeholder='Your Name' type='text' id='name' name='name' required />
+            <input className='bg-dark-theme  placeholder-textgray py-2 pl-4 font-josefin text-xs sm:text-sm sm:pr-10 md:text-lg md:pr-20 outline-none' placeholder='Your Name' type='text' id='name' name='name' required />
           </div>
           <div>
-            <input className='bg-dark-theme  placeholder-textgray py-2 pl-4 font-josefin text-xs outline-none' placeholder='Email' type='email' id='email' name='email' required />
+            <input className='bg-dark-theme  placeholder-textgray py-2 pl-4 font-josefin text-xs sm:text-sm sm:pr-10 md:text-lg md:pr-20  outline-none' placeholder='Email' type='email' id='email' name='email' required />
           </div>
           <div>
-            <input className='bg-dark-theme  placeholder-textgray py-2 pl-4 font-josefin text-xs outline-none' placeholder='Phone Number' type='tel' id='phone' name='phone' required />
+            <input className='bg-dark-theme  placeholder-textgray py-2 pl-4 font-josefin text-xs sm:text-sm sm:pr-10 md:text-lg md:pr-20  outline-none' placeholder='Phone Number' type='tel' id='phone' name='phone' required />
           </div>
           <div>
-            <input className='bg-dark-theme  placeholder-textgray py-2 pl-4 font-josefin text-xs outline-none' placeholder='Subject' type='text' id='subject' name='subject' required />
+            <input className='bg-dark-theme  placeholder-textgray py-2 pl-4 font-josefin text-xs sm:text-sm sm:pr-10 md:text-lg md:pr-20  outline-none' placeholder='Subject' type='text' id='subject' name='subject' required />
           </div>
           <div>
-            <textarea className='bg-dark-theme placeholder-textgray py-2 pl-4 font-josefin text-xs outline-none' placeholder='Message' id='message' name='message' required></textarea>
+            <textarea className='bg-dark-theme placeholder-textgray py-2 pl-4 font-josefin text-xs sm:text-sm sm:pr-10 md:text-lg md:pr-20  outline-none' placeholder='Message' id='message' name='message' required></textarea>
           </div>
           <div>
-            <button type='submit' className='bg-darker-theme px-6 py-2 text-xs rounded-xl font-josefin'>Submit</button>
+            <button type='submit' className='bg-darker-theme px-6 py-2 text-xs sm:text-sm md:text-lg rounded-xl font-josefin'>Submit</button>
           </div>
         </form>
       </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,12 +5,12 @@ import Email from '../assets/Email.png'
 
 const Footer = () => {
   return (
-    <div className='flex flex-col bg-darker-theme py-4 justify-center items-center text-center gap-3'>
-      <h1 className='font-josefin text-md'>DevNito</h1>
+    <div className='flex flex-col bg-darker-theme py-4 justify-center items-center text-center gap-3 sm:gap-2'>
+      <h1 className='font-josefin text-md sm:text-lg md:text-xl'>DevNito</h1>
       <div className='socials flex gap-3'>
-        <a href=""> <img className='w-4 h-4' src={Facebook} alt="" /></a>
-        <a href=""> <img className='w-4 h-4' src={Linkedin} alt="" /></a>
-        <a href=""> <img className='w-4 h-4' src={Email} alt=""    /></a>
+        <a href=""> <img className='w-4 h-4 sm:w-5 sm:h-5 md:w-7 md:h-7' src={Facebook} alt="" /></a>
+        <a href=""> <img className='w-4 h-4 sm:w-5 sm:h-5 md:w-7 md:h-7' src={Linkedin} alt="" /></a>
+        <a href=""> <img className='w-4 h-4 sm:w-5 sm:h-5 md:w-7 md:h-7' src={Email} alt=""    /></a>
       </div>
     </div>
   )
