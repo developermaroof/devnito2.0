96fe20a36ffd452d25a6c979ed527a5b8a8bef77
Maroof Qureshi <developermaroof@gmail.com>
2024-06-28 19:04:27 +0500

    Updated

--- src/components/Address.jsx
+++ src/components/Address.jsx
@@ -4,10 +4,10 @@ import Map from '../assets/Map.png'
 const Address = () => {
   return (
     <div className='Address bg-darker-theme my-10 font-josefin'>
-      <div className='p-8 flex flex-col justify-center items-center text-center'>
-        <h1 className='mb-2'>Address</h1>
-        <p className='text-gray-500 text-xs'>Meydan Hotel, Nad Al-Sheba,</p>
-        <p className='text-gray-500 text-xs'>Dubai - UAE</p>
+      <div className='p-8 sm:p-16 md:p-20 flex flex-col justify-center items-center text-center'>
+        <h1 className='mb-2 sm:text-xl md:text-3xl'>Address</h1>
+        <p className='text-gray-500 text-xs sm:text-sm md:text-lg'>Meydan Hotel, Nad Al-Sheba,</p>
+        <p className='text-gray-500 text-xs sm:text-sm md:text-lg'>Dubai - UAE</p>
         <a href="https://www.google.com/maps?ll=25.152337,55.30809&z=15&t=m&hl=en-US&gl=US&mapclient=embed&cid=4935961900776118242">
         <img src={Map} alt="" className='mt-4'/></a></div>
     </div>
--- src/components/ContactDevnito.jsx
+++ src/components/ContactDevnito.jsx
@@ -6,39 +6,39 @@ import Phone from '../assets/mobilePhone.png'
 
 const ContactDevnito = () => {
   return (
-    <div className='ContactDevnito p-6 font-josefin'>
-      <h1 className='px-4 font-bold text-lg'>Contact DevNito</h1>
-      <p className='px-4 text-xs text-gray-400'>Get in Touch with Us</p>
-      <div className='ContactImg  flex flex-col mt-10'>  
-          <form className='gap-4 ml-6 mt-4 p-4 flex justify-center items-center w-40 h-auto flex-col bg-dark-theme'>
-            <h1 className='text-xs white font-bold'>LEAVE US MESSAGE</h1>
+    <div className='ContactDevnito p-6 sm:px-10 md:p-12 font-josefin'>
+      <h1 className='px-4 sm:px-10 md:px-12 font-bold text-lg sm:text-2xl'>Contact DevNito</h1>
+      <p className='px-4 sm:px-10 md:px-12 text-xs sm:text-sm text-gray-400'>Get in Touch with Us</p>
+      <div className='ContactImg  flex flex-col mt-10'>
+          <form className='gap-4 md:gap-8 ml-6 sm:ml-10 mt-4 sm:mt-8 p-4 sm:py-8 md:py-10 flex justify-center items-center w-40 sm:w-52 md:w-60 h-auto flex-col bg-dark-theme'>
+            <h1 className='text-xs sm:text-sm white font-bold'>LEAVE US MESSAGE</h1>
             <div>
-              <input className='bg-dark-theme placeholder-textgray font-josefin text-xs outline-none border-b-2 border-gray-600 w-full' placeholder='Name' type='text' id='name' name='name' required />
+              <input className='bg-dark-theme placeholder-textgray font-josefin text-xs md:text-sm outline-none border-b-2 border-gray-600 w-full md:w-52' placeholder='Name' type='text' id='name' name='name' required />
             </div>
             <div>
-              <input className='bg-dark-theme placeholder-textgray font-josefin text-xs outline-none border-b-2 border-gray-600 w-full' placeholder='Email' type='email' id='email' name='email' required />
+              <input className='bg-dark-theme placeholder-textgray font-josefin text-xs md:text-sm outline-none border-b-2 border-gray-600 w-full md:w-52' placeholder='Email' type='email' id='email' name='email' required />
             </div>
             <div>
-              <textarea className='bg-dark-theme placeholder-textgray font-josefin text-xs outline-none border-b-2 border-gray-600 w-full' placeholder='Message' id='message' name='message' required></textarea>
+              <textarea className='bg-dark-theme placeholder-textgray font-josefin text-xs md:text-sm outline-none border-b-2 border-gray-600 w-full md:w-52' placeholder='Message' id='message' name='message' required></textarea>
             </div>
             <div>
-              <button type='submit' className='bg-darker-theme py-2 px-12 text-xs font-josefin'>Send</button>
+              <button type='submit' className='bg-darker-theme py-2 px-12 text-xs md:text-sm font-josefin'>Send</button>
             </div>
           </form>
         
-        <div className='contacts flex flex-col gap-2 justify-start items-start  mx-8 mt-4 mb-4'>
+        <div className='contacts sm:ml-12 sm:mb-12 flex flex-col gap-2 justify-start items-start  mx-8 mt-4 mb-4'>
           <div className='flex justify-center items-center'>
-            <img src={Phone} alt="Phone" className='w-6 h-6'/>
+            <img src={Phone} alt="Phone" className='w-6 h-6 md:w-8 md:h-8'/>
             <div className='flex flex-col'>
-              <span className='text-xs'>Phone</span>
-              <span className='text-xs text-gray-400'>+971501579610</span>
+              <span className='text-xs md:text-sm'>Phone</span>
+              <span className='text-xs md:text-sm text-gray-400'>+971501579610</span>
             </div>
           </div>
           <div  className='flex pl-1 justify-center items-center'>
-            <img src={Email} alt="Email" className='w-4 h-4'/>
+            <img src={Email} alt="Email" className='w-4 h-4 md:w-6 md:h-6'/>
             <div className='flex flex-col pl-2'>
-              <span className='text-xs'>Email</span>
-              <span className='text-xs text-gray-400'>contact@devnito.com</span>
+              <span className='text-xs md:text-sm'>Email</span>
+              <span className='text-xs md:text-sm text-gray-400'>contact@devnito.com</span>
             </div>
           </div>
         </div>
