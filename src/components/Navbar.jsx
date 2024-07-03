39881f5345d874470c2fb5a205b07c5389b178b7
Maroof Qureshi <developermaroof@gmail.com>
2024-07-03 12:33:01 +0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,13 +30,13 @@ const Navbar = () => {
   }, []);
 
   return (
-    <nav className={`Navbar ${isScrolled ? 'bg-darker-theme' : 'bg-dark-theme'} z-50 justify-center items-center flex py-4 sticky top-0 transition-colors duration-300`}>
+    <nav className={`Navbar ${isScrolled ? 'bg-darker-theme' : 'bg-dark-theme'} z-50 justify-center items-center flex py-4 sticky top-0 transition-colors duration-300 xl:max-w-screen-xl xl:mx-auto`}>
       <div className="content flex items-center justify-between w-full px-4">
         <div className={`logdiv items-center justify-center ${isScrolled ? 'bg-darker-theme' : ''} transition-colors duration-300`}>
           <img 
             src={Logo} 
             alt="Company Logo" 
-            className={`logo-img sm:w-32 md:w-36 w-24 h-auto ${isScrolled ? 'filter brightness-75' : ''} transition-filter duration-300`} 
+            className={`logo-img sm:w-32 md:w-36 w-24 h-auto lg:w-56 lg:p-4 ${isScrolled ? 'filter brightness-75' : ''} transition-filter duration-300`} 
           />
         </div>
         <div className='justify-center gap-10 items-center flex lg:hidden'>
@@ -65,17 +65,17 @@ const Navbar = () => {
             )}
           </div>
         </div>
-        <div className='hidden lg:flex lg:gap-10 lg:items-center'>
+        <div className='hidden lg:flex lg:gap-4 lg:items-center'>
         <div className='modesdiv flex items-center justify-center gap-1'>
-            <img src={ModeBlack} alt="Mode Icon" className="mode-icon w-2 h-2 sm:w-3 sm:h-3 md:w-4 md:h-4" />
-            <span className='text-white text-xs sm:text-sm md:text-md'>Modes</span>
-            <img src={DownArrow} alt="Dropdown Arrow" className="down-arrow w-2 h-2 sm:w-3 sm:h-3 md:w-4 md:h-4" />
+            <img src={ModeBlack} alt="Mode Icon" className="mode-icon w-2 h-2 sm:w-3 sm:h-3 md:w-4 md:h-4 lg:w-5 lg:h-5" />
+            <span className='text-white text-xs sm:text-sm md:text-md lg:text-lg'>Modes</span>
+            <img src={DownArrow} alt="Dropdown Arrow" className="down-arrow w-2 h-2 sm:w-3 sm:h-3 md:w-4 md:h-4 lg:w-5 lg:h-5" />
           </div>
-          <Link to="/" className='text-white lg:px-4 lg:py-2 hover:bg-gray-700 rounded transition-colors duration-300'>Home</Link>
-          <Link to="/about" className='text-white lg:px-4 lg:py-2 hover:bg-gray-700 rounded transition-colors duration-300'>About</Link>
-          <Link to="/contact" className='text-white lg:px-4 lg:py-2 hover:bg-gray-700 rounded transition-colors duration-300'>Contact</Link>
-          <Link to="/projects" className='text-white lg:px-4 lg:py-2 hover:bg-gray-700 rounded transition-colors duration-300'>Projects</Link>
-          <Link to="/services" className='text-white lg:px-4 lg:py-2 hover:bg-gray-700 rounded transition-colors duration-300'>Services</Link>
+          <Link to="/" className='text-white lg:px-4 lg:py-2 lg:text-lg lg:ml-10 hover:bg-gray-700 rounded transition-colors duration-300 lg:text-gray-300'>Home</Link>
+          <Link to="/about" className='text-white lg:px-4 lg:py-2 lg:text-lg hover:bg-gray-700 rounded transition-colors duration-300 lg:text-gray-300'>About</Link>
+          <Link to="/contact" className='text-white lg:px-4 lg:py-2 lg:text-lg hover:bg-gray-700 rounded transition-colors duration-300 lg:text-gray-300'>Contact</Link>
+          <Link to="/projects" className='text-white lg:px-4 lg:py-2 lg:text-lg hover:bg-gray-700 rounded transition-colors duration-300 lg:text-gray-300'>Projects</Link>
+          <Link to="/services" className='text-white lg:px-4 lg:py-2 lg:text-lg hover:bg-gray-700 rounded transition-colors duration-300 lg:text-gray-300'>Services</Link>
         </div>
       </div>
     </nav>
