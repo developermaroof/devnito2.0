c5a98c9456d49a17ba63e8e4051039536822dc11
Maroof Qureshi <developermaroof@gmail.com>
2024-07-04 20:59:37 +0500

    Updated some files

--- src/components/Transform.jsx
+++ src/components/Transform.jsx
@@ -2,10 +2,8 @@ import React from 'react'
 
 const Transform = () => {
   return (
-    <div className='2xl:max-w-screen-2xl 2xl:mx-auto Transform bg-darker-theme gap-4 py-16 p-6 justify-center flex flex-col text-center items-center'>
-      <h1 className='font-bold text-sm font-josefin sm:text-lg md:text-xl'>Transforming businesses through exceptional IT solutions
-</h1>
-      
+    <div className='2xl:max-w-screen-2xl 2xl:mx-auto Transform bg-darker-theme gap-4 py-16 p-6 justify-center flex flex-col text-center items-center lg:px-52'>
+      <h1 className='font-bold text-sm font-josefin sm:text-lg md:text-xl lg:text-3xl'>Transforming businesses through exceptional IT solutions</h1>
     </div>
   )
 }
--- src/components/WhyChoose.jsx
+++ src/components/WhyChoose.jsx
@@ -3,12 +3,16 @@ import WhyChooseWe from '../assets/WhyChoose.png'
 
 const WhyChoose = () => {
   return (
-    <div className='2xl:max-w-screen-2xl 2xl:mx-auto WhyChoose bg-darker-theme gap-5 py-16 p-6 justify-center flex flex-col text-center items-center'>
-      <p className='text-xs font-thin font-josefin sm:text-sm'>AND LET'S NOT FORGET
+    <div className='lg:gap-20 2xl:max-w-screen-2xl 2xl:mx-auto WhyChoose bg-darker-theme gap-5 py-16 p-6 justify-center flex flex-col text-center items-center lg:flex-row lg:px-10'>
+     <div className='lg:flex lg:flex-col lg:gap-4 '>
+       <p className='text-xs font-thin font-josefin sm:text-sm lg:text-lg lg:text-left lg:font-normal lg:text-gray-300'>AND LET'S NOT FORGET
       </p>
-      <h1 className='font-bold text-xl font-josefin sm:text-2xl md:text-3xl'>Why Choose DevNito</h1>
-      <p className='text-xs font-thin font-josefin sm:text-sm'>At DevNito, we prioritize transparent collaboration and client-centricity to ensure your success. With our exceptional IT export services, freelancing connections, and software sales, we revolutionize businesses by providing innovative solutions. Connect with us today and elevate your business to new heights.</p>
+      <h1 className='font-bold text-xl font-josefin sm:text-2xl md:text-3xl lg:text-4xl lg:text-left'>Why Choose DevNito</h1>
+      <p className='text-xs font-thin font-josefin sm:text-sm lg:text-lg lg:text-left lg:font-normal lg:text-gray-300'>At DevNito, we prioritize transparent collaboration and client-centricity to ensure your success. With our exceptional IT export services, freelancing connections, and software sales, we revolutionize businesses by providing innovative solutions. Connect with us today and elevate your business to new heights.</p>
+    </div> 
+    <div className='lg:w-full'>
       <img src={WhyChooseWe} alt="" />
+    </div>
     </div>
   )
 }
