37a7edfd9700cfd10eb84a873fc69585da5aa49d
Maroof Qureshi <developermaroof@gmail.com>
2024-07-20 12:48:20 +0500

    Updated

--- src/components/TeamDesktop.jsx
+++ src/components/TeamDesktop.jsx
@@ -7,17 +7,17 @@ import abdullah from '../assets/abdullah.png'
 const TeamDesktop = () => {
 
   return (
-    <div className='bg-darker-theme'>
+    <div className='dark:bg-darker-theme bg-white'>
     <div className='Team  flex flex-col items-center text-center 2xl:max-w-screen-2xl 2xl:mx-auto'>
-      <div className='bg-dark-theme py-8 px-10'>
+      <div className='dark:bg-dark-theme bg-white py-8 px-10'>
         <img src={teamImg} alt="" />
       </div>
         {/*  */}
-      <div className='bg-darker-theme pb-10 px-10  flex flex-col gap-32 w-full justify-center items-center'>
-        <h1 className='bg-dark-theme text-2xl font-bold font-josefin w-80 py-6 rounded-b-2xl'>Meet the best team ever</h1>
+      <div className='dark:bg-darker-theme bg-light-theme-silver pb-10 px-10  flex flex-col gap-32 w-full justify-center items-center'>
+        <h1 className='dark:bg-dark-theme bg-light-theme-black text-2xl font-bold font-josefin w-80 py-6 rounded-b-2xl'>Meet the best team ever</h1>
         <div className='teamMember flex flex-row gap-20'>
         {/*  */}
-          <div className='member1 bg-dark-theme px-10 pb-6 pt-20  relative '>
+          <div className='member1 dark:bg-dark-theme bg-light-theme-black px-10 pb-6 pt-20  relative '>
             <div className='absolute -top-20  left-1/2 transform -translate-x-1/2  rounded-xl p-2 w-44'>
               <img src={junaid} alt="" />
             </div>
@@ -27,7 +27,7 @@ const TeamDesktop = () => {
           
           </div>
 {/*  */}
-        <div className='member1 bg-dark-theme px-10 pb-6 pt-20  relative '>
+        <div className='member1 dark:bg-dark-theme bg-light-theme-black px-10 pb-6 pt-20  relative '>
         <div className='absolute -top-20  left-1/2 transform -translate-x-1/2  rounded-xl p-2 w-44 '>
             <img src={vincent} alt="" />
         </div>
@@ -35,7 +35,7 @@ const TeamDesktop = () => {
           <p className='font-bold'>Financial Advisor</p>
         </div>
 {/*  */}
-        <div className='member1 bg-dark-theme px-10 pb-6 pt-20  relative '>
+        <div className='member1 dark:bg-dark-theme bg-light-theme-black px-10 pb-6 pt-20  relative '>
         <div className='absolute -top-20  left-1/2 transform -translate-x-1/2  rounded-xl p-2 w-44 '>
             <img src={abdullah} alt="" />
         </div>
