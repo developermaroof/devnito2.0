ab9947c2f55670b20666b01b4857c51122c8ed1e
Maroof Qureshi <developermaroof@gmail.com>
2024-07-22 13:02:49 +0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,9 +10,9 @@ const Footer = () => {
     <div className='2xl:max-w-screen-2xl 2xl:mx-auto flex flex-col py-4 justify-center items-center text-center gap-3 sm:gap-2'>
       <h1 className='font-josefin text-md sm:text-lg md:text-xl'>DevNito</h1>
       <div className='socials flex gap-3'>
-        <a href=""> <img className='w-4 h-4 sm:w-5 sm:h-5 md:w-7 md:h-7' src={Facebook} alt="" /></a>
-        <a href=""> <img className='w-4 h-4 sm:w-5 sm:h-5 md:w-7 md:h-7' src={Linkedin} alt="" /></a>
-        <a href=""> <img className='w-4 h-4 sm:w-5 sm:h-5 md:w-7 md:h-7' src={Email} alt=""    /></a>
+        <a> <img className='w-4 h-4 sm:w-5 sm:h-5 md:w-7 md:h-7' src={Facebook} alt="" /></a>
+        <a> <img className='w-4 h-4 sm:w-5 sm:h-5 md:w-7 md:h-7' src={Linkedin} alt="" /></a>
+        <a> <img className='w-4 h-4 sm:w-5 sm:h-5 md:w-7 md:h-7' src={Email} alt=""    /></a>
       </div>
     </div>
     </div>
